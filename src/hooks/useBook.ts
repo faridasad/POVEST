578581feec22cc553e21981c8dc7b78bb7950a5b
faridasad <asadlifaridd@gmail.com>
2023-11-03 21:37:08 +0400

    Add hook for pagination

---
+++ src/hooks/useBook.ts
@@ -0,0 +1,31 @@
+import { ReactElement, useState } from "react";
+
+export function useBook(steps: ReactElement[]) {
+  const [stepIndex, setStepIndex] = useState(0);
+
+  const next = () => {
+    setStepIndex((i) => {
+      return i >= steps.length - 1 ? i : i + 1;
+    });
+  };
+
+  const prev = () => {
+    setStepIndex((i) => {
+      return i <= 0 ? i : i - 1;
+    });
+  };
+
+  const moveTo = (index: number) => {
+    setStepIndex(index);
+  };
+
+  return {
+    stepIndex,
+    moveTo,
+    next,
+    prev,
+    step: steps[stepIndex],
+    isLastStep: stepIndex === steps.length - 1,
+    isFirstStep: stepIndex === 0,
+  };
+}
